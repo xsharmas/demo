327a297fac7bcb42d074bf11cdcd4689d76e5c1c
xsharmas <mayank110078@gmail.com>
2025-07-07 01:11:50 +0530

    third changes

--- test.py
+++ test.py
@@ -2,5 +2,5 @@ print("heloo world")
 print("This is a test file.")
 # print("This file is used to test git functionality.")
 #sumof two numbers
-def sum(a, b):
+def sum(20, 40):
     return a + b   
