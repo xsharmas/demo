17ff1663a14ea309466eb48aa4f5d3aac1836157
xsharmas <mayank110078@gmail.com>
2025-07-07 01:35:41 +0530

    4th commit

--- test.py
+++ test.py
@@ -2,5 +2,12 @@ print("heloo world")
 print("This is a test file.")
 # print("This file is used to test git functionality.")
 #sumof two numbers
-def sum(20, 40):
-    return a + b   
+def sum_of_two_numbers():
+    a = int(input("Enter first number: "))
+    b = int(input("Enter second number: "))
+    return a + b
+multiplication by getting user input
+def multiply():
+    a = int(input("Enter first number: "))
+    b = int(input("Enter second number: "))
+    return a * b
